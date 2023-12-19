174de2a79b0c8e214654f6c1cdac72c31be318cd
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2023-12-19 15:53:21 +0200

    Add FlagixWidget to conditionally show child widget based on user's role and required permission

---
+++ lib/src/flagix_widget.dart
@@ -0,0 +1,48 @@
+import 'package:feature_flagix/src/app_flagix.dart';
+import 'package:flutter/material.dart';
+
+/// A widget that conditionally shows its child
+/// based on the user's role and required permission.
+class FlagixWidget<P> extends StatefulWidget {
+  /// Constructs a [FlagixWidget].
+  ///
+  /// The [flagix] parameter specifies the flag
+  /// required to show the child widget.
+  ///
+  /// The [child] parameter specifies the widget to be shown if
+  /// the user has the required permission.
+  ///
+  const FlagixWidget({
+    required this.flagix,
+    required this.child,
+    super.key,
+  });
+
+  /// The required permission to show the child widget.
+  final P flagix;
+
+  /// The child widget to be shown if the user has the required permission.
+  final Widget child;
+
+  @override
+  State<FlagixWidget> createState() => _FlagixWidgetState();
+}
+
+class _FlagixWidgetState extends State<FlagixWidget> {
+  final AppFlagix _appFlagix = AppFlagix();
+  late String role;
+  late bool hasPermissionGranted;
+
+  @override
+  void initState() {
+    super.initState();
+    role = _appFlagix.currentRole;
+    hasPermissionGranted = _appFlagix.hasPermission(widget.flagix);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Show the widget only if the role has the required permission
+    return hasPermissionGranted ? widget.child : const SizedBox.shrink();
+  }
+}
