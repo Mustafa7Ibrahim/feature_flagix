021e40c8d08a1e55b5dee99ecf85379010d2aa11
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2023-12-24 14:28:00 +0200

    Refactor FeatureFlagix class to use app flags instead of app permissions

--- lib/src/feature_flagix.dart
+++ lib/src/feature_flagix.dart
@@ -1,7 +1,7 @@
 import 'package:feature_flagix/src/models/flagix_roles.dart';
 
 class FeatureFlagix {
-  FlagixRoles? _appPermissions; // Nullable
+  FlagixRoles? _appFlags; // Nullable
   dynamic _currentRole;
 
   // Private constructor to prevent instantiation from outside the class.
@@ -19,23 +19,32 @@ class FeatureFlagix {
   dynamic get currentRole => _currentRole;
 
   /// Sets the app permissions
-  void setPermissions(FlagixRoles permissions) => _appPermissions = permissions;
+  void setFlags(FlagixRoles flags) => _appFlags = flags;
 
   /// Sets the current role.
   void setCurrentRole(dynamic role) => _currentRole = role;
 
   bool hasPermission(dynamic permission) {
     // Check if _appPermissions is initialized
-    if (_appPermissions == null) {
+    if (_appFlags == null) {
       throw Exception(
-        "App permissions not initialized. Call setPermissions method.",
+        """
+        App flags not initialized. Call setFlags method.
+
+        Make sure you call setFlags() method before calling hasPermission method.
+        and call setCurrentRole() method before calling hasPermission method.
+
+        Example:
+            FeatureFlagix().setFlags(appFlags);
+            FeatureFlagix().setCurrentRole('admin');
+        """,
       );
     }
 
     // Check if the current role exists in the appPermissions
-    if (_appPermissions!.roles.containsKey(_currentRole)) {
+    if (_appFlags!.roles.containsKey(_currentRole)) {
       // Get the permissions associated with the current role
-      final rolePermissions = _appPermissions!.roles[_currentRole]!.permissions;
+      final rolePermissions = _appFlags!.roles[_currentRole]!.permissions;
 
       // Check if the specified permission is present in the role's permissions
       return rolePermissions.contains(permission);
